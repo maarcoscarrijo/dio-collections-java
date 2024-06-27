cfde9302b4063850ebdfbe86da111b556780d7df
maarcoscarrijo <carrijo.x@hotmail.com>
2024-06-27 20:05:00 -0300

    feat: Resolvendo desafio Set, Agenda Contatos
    Signed-off-by: maarcoscarrijo <carrijo.x@hotmail.com>

---
+++ src/set/Pesquisa/Contato.java
@@ -0,0 +1,44 @@
+package set.Pesquisa;
+
+import java.util.Objects;
+
+public class Contato {
+    private String nome;
+    private int numero;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contato contato)) return false;
+        return getNome().equals(contato.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome());
+    }
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "{ Contato: " + nome +
+                ", " + numero +
+                '}';
+    }
+}
