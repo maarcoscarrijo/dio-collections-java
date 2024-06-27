076a5b725d406386afaf197114eb82405bd08a56
maarcoscarrijo <carrijo.x@hotmail.com>
2024-06-27 20:29:51 -0300

    feat: Resolvendo desafio Set, Lista de Tarefas
    Signed-off-by: maarcoscarrijo <carrijo.x@hotmail.com>

---
+++ src/set/Pesquisa/ListaTarefa.java
@@ -0,0 +1,127 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaTarefa {
+    private Set<Tarefa> tarefaSet;
+
+    public ListaTarefa() {
+        this.tarefaSet = new HashSet<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaSet.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        Tarefa tarefaParaRemover = null;
+        if (!tarefaSet.isEmpty()) {
+            for (Tarefa t : tarefaSet) {
+                if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                    tarefaParaRemover = t;
+                    break;
+                }
+            }
+            tarefaSet.remove(tarefaParaRemover);
+        } else {
+            System.out.println("Conjuto está vazio!");
+        }
+    }
+
+    public void exibirTarefa() {
+        if (!tarefaSet.isEmpty()) {
+            System.out.println(tarefaSet);
+        } else {
+            System.out.println("Tarefa não encontrada!");
+        }
+    }
+
+    public int contarTarefas() {
+        return tarefaSet.size();
+    }
+
+    public Set<Tarefa> obterTarefasTrue() {
+        Set<Tarefa> tarefasConcluidas = new HashSet<>();
+        for (Tarefa t : tarefaSet) {
+            if (t.isConcluida()) {
+                tarefasConcluidas.add(t);
+            }
+        }
+        return tarefasConcluidas;
+    }
+
+    public Set<Tarefa> obterTarefasPendentes() {
+        Set<Tarefa> tarefasNaoConcluidas = new HashSet<>();
+        for (Tarefa t : tarefaSet) {
+            if (!t.isConcluida()) {
+                tarefasNaoConcluidas.add(t);
+            }
+        }
+        return tarefasNaoConcluidas;
+    }
+
+    public void marcarTarefaConcluida(String descricao) {
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                t.setConcluida(true);
+            }
+        }
+    }
+
+    public void marcarTarefaPendente(String descricao) {
+        Tarefa tarefaParaMarcarComoPendente = null;
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaMarcarComoPendente = t;
+                break;
+            }
+        }
+
+        if (tarefaParaMarcarComoPendente != null) {
+            if(tarefaParaMarcarComoPendente.isConcluida()) {
+                tarefaParaMarcarComoPendente.setConcluida(false);
+            }
+        } else {
+            System.out.println("Tarefa não encontrada na lista.");
+        }
+    }
+
+    public void limparListaTarefas() {
+        if(tarefaSet.isEmpty()) {
+            System.out.println("A lista já está vazia!");
+        } else {
+            tarefaSet.clear();
+        }
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefas = new ListaTarefa();
+
+        listaTarefas.adicionarTarefa("Estudar Java");
+        listaTarefas.adicionarTarefa("Fazer exercícios físicos");
+        listaTarefas.adicionarTarefa("Organizar a mesa de trabalho");
+        listaTarefas.adicionarTarefa("Ler livro");
+        listaTarefas.adicionarTarefa("Preparar apresentação");
+
+        listaTarefas.exibirTarefa();
+
+        listaTarefas.removerTarefa("Fazer exercícios físicos");
+        listaTarefas.exibirTarefa();
+
+        System.out.println("Total de tarefas na lista: " + listaTarefas.contarTarefas());
+
+        System.out.println(listaTarefas.obterTarefasPendentes());
+
+        listaTarefas.marcarTarefaConcluida("Ler livro");
+        listaTarefas.marcarTarefaConcluida("Estudar Java");
+
+        System.out.println(listaTarefas.obterTarefasTrue());
+
+        listaTarefas.marcarTarefaPendente("Estudar Java");
+        listaTarefas.exibirTarefa();
+
+        listaTarefas.limparListaTarefas();
+        listaTarefas.exibirTarefa();
+    }
+}
---
+++ src/set/Pesquisa/Tarefa.java
@@ -0,0 +1,34 @@
+package set.Pesquisa;
+
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "\nDescrição da tarefa: " + descricao + '\'' +
+                ", concluida? " + concluida;
+    }
+}
